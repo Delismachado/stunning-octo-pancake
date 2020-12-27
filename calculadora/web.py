ce177d806289ebd8fa15fd4c819f1d77d271b2a0
Derli Machado <dam.machado1@gmail.com>
2020-12-27 09:27:22 -0300

    Add: template and styles.

--- calculadora/web.py
+++ calculadora/web.py
@@ -1,60 +1,48 @@
 # pip3 install flask
-from flask import  Flask
+from flask import  Flask, render_template
 from calc import soma, subtracao, divisao, multiplicacao
 
 app = Flask(__name__)
 
 h1 = '<h1>Calculadora...</h1>'
+titulo = 'caluladora olist'
 
 @app.route('/')
 
+
 def index():
-  h1 = '<h1>Calculadora Olist</h1>'
-  ol = '''
-          <ol>
-            <li><a href="/soma">Somar</a></li>
-            <li><a href="/subtracao">Subtrair</a></li>
-            <li><a href="/divisao">Divisão</a></li>
-            <li><a href="/multiplicar">Multiplicar</a></li>          
-          </ol>  
-       '''
-  return f'{h1} {ol}'
+  lista = ['somar', 'subtrair', 'dividir']
+  return render_template('index.html', titulo=titulo)
+ 
 
 @app.route('/soma')
 def soma_cal():
-  n1 = 3.3
-  n2 = 1.4
+  n1 = 3.0
+  n2 = 5.0
   resultado = soma(n1, n2) 
-  h2 = f'<h2> A soma é: {resultado}</h2>'
-  voltar = '<a href="/">Voltar</a>'
-  return f'{h1} {h2} <br> {voltar}'
+  return render_template('somar.html', resultado=resultado, titulo=titulo)
 
 @app.route('/subtracao')
 def subtracao_cal():
   n1 = 3.3
   n2 = 1.4
-  resultado = subtracao(n1, n2)  
-  h2 = f'<h2> A subtracao é: {resultado}</h2>'
-  voltar = '<a href="/">Voltar</a>'
-  return f'{h1} {h2} <br> {voltar}'
+  resultado = subtracao(n1, n2)
+  return render_template('subtrair.html', resultado=resultado, titulo=titulo)  
+  
 
 @app.route('/divisao')
 def divisao_cal():
   n1 = 3.3
   n2 = 1.4
   resultado = divisao(n1, n2)
+  return render_template('dividir.html', resultado=resultado, titulo=titulo)
  
-  h2 = f'<h2> A soma é: {resultado}</h2>'
-  voltar = '<a href="/">Voltar</a>'
-  return f'{h1} {h2} <br> {voltar}'
-
+  
 @app.route('/multiplicar')
 def multiplicacao_cal():
   n1 = 3.3
   n2 = 1.4
   resultado = multiplicacao(n1, n2) 
-  h2 = f'<h2> A soma é: {resultado}</h2>'
-  voltar = '<a href="/">Voltar</a>'
-  return f'{h1} {h2} <br> {voltar}'
+  return render_template('multiplicar.html', resultado=resultado, titulo=titulo)
 
 app.run(debug = True)
